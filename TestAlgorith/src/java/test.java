0e5198f8f5600eecc8b8d1b6d13f91cb40aff83e
cyc1614 <83817782+cyc1614@users.noreply.github.com>
2021-09-24 03:05:01 +0800

    4

---
+++ TestAlgorith/src/java/test.java
@@ -0,0 +1,13 @@
+package java;
+
+/**
+ * @Auther: cyc
+ * @Date: 2021/9/24 - 09 - 24
+ * @Description: java
+ * @version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("444");
+    }
+}
